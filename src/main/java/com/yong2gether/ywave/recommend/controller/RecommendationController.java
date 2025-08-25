797fc9ed6548ede3763b54f0529032681f2fcfc6
wldh25 <liho858@gmail.com>
2025-08-25 22:25:13 +0900

    Docs : API 관련 설명 추가

--- src/main/java/com/yong2gether/ywave/recommend/controller/RecommendationController.java
+++ src/main/java/com/yong2gether/ywave/recommend/controller/RecommendationController.java
@@ -2,6 +2,7 @@ package com.yong2gether.ywave.recommend.controller;
 
 import com.yong2gether.ywave.recommend.dto.RecommendedStore;
 import com.yong2gether.ywave.recommend.service.RecommendationService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +13,14 @@ import java.security.Principal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/recommendations")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class RecommendationController {
 
     private final RecommendationService service;
 
-    @GetMapping
+    @Operation(summary = "사용자가 입력한 선호 카테고리 기반 AI 추천 API", description = "사용자가 입력한 선호 카테고리 정보에 속하는 가맹점들을 추천해줍니다.")
+    @GetMapping("/recommendations")
     public ResponseEntity<List<RecommendedStore>> recommend(
             Principal principal,
             @RequestParam(defaultValue = "5") @Min(1) @Max(20) int limit
